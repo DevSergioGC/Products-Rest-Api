e19003ea82d51b3e17346c64cd3ffe26ef24b7ec
Sergio Guzmán <82333906+DnaCyphers@users.noreply.github.com>
2023-02-23 20:41:50 -0400

    Create DB models
    Add jwt, levels, product_user, product & user db models. Initialize all of them.

---
+++ api/models/product_user.py
@@ -0,0 +1,16 @@
+from api.db import db
+
+class ProductUser(db.Model):
+    __tablename__ = 'product_user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
